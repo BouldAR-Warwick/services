66dc08f793133d0dea92a2011b2f5172bf6d5944
Thomas Cowley <9660001+tmcowley@users.noreply.github.com>
2023-04-24 00:04:05 +0100

    removing snapshot and pom bump

--- scripts/make.sh
+++ scripts/make.sh
@@ -7,15 +7,15 @@ if [ ! -d ~/tomcat-10.0.27/ ]; then
 fi
 
 # remove war files from server
-rm -rv ~/tomcat-10.0.27/webapps/webservices-1.0-SNAPSHOT
-rm -v ~/tomcat-10.0.27/webapps/webservices-1.0-SNAPSHOT.war
+rm -rv ~/tomcat-10.0.27/webapps/webservices-1.0
+rm -v ~/tomcat-10.0.27/webapps/webservices-1.0.war
 
 # clean and make war
 mvn --file ~/Projects/pbrg_services/pom.xml clean package -Dmaven.test.skip
-# chmod 755 ~/Projects/pbrg_services/target/webservices-1.0-SNAPSHOT.war
+# chmod 755 ~/Projects/pbrg_services/target/webservices-1.0.war
 
 # migrate war to tomcat server
-mv ~/Projects/pbrg_services/target/webservices-1.0-SNAPSHOT.war ~/tomcat-10.0.27/webapps/
+mv ~/Projects/pbrg_services/target/webservices-1.0.war ~/tomcat-10.0.27/webapps/
 
 # run tomcat startup (ensure refresh)
 # source ~/tomcat-10.0.27/bin/startup.sh
