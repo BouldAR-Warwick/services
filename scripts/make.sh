da61cb5497274f1f26ae767b46879a2a8b64842f
Thomas Cowley <9660001+tmcowley@users.noreply.github.com>
2023-04-24 15:24:17 +0100

    Make script update

--- scripts/make.sh
+++ scripts/make.sh
@@ -11,11 +11,11 @@ rm -rv ~/tomcat-10.0.27/webapps/webservices-1.0
 rm -v ~/tomcat-10.0.27/webapps/webservices-1.0.war
 
 # clean and make war
-mvn --file ~/Projects/pbrg_services/pom.xml clean package -Dmaven.test.skip
-# chmod 755 ~/Projects/pbrg_services/target/webservices-1.0.war
+mvn --file ~/Projects/services/pom.xml clean package -Dmaven.test.skip
+# chmod 755 ~/Projects/services/target/webservices-1.0.war
 
 # migrate war to tomcat server
-mv ~/Projects/pbrg_services/target/webservices-1.0.war ~/tomcat-10.0.27/webapps/
+mv ~/Projects/services/target/webservices-1.0.war ~/tomcat-10.0.27/webapps/
 
 # run tomcat startup (ensure refresh)
 # source ~/tomcat-10.0.27/bin/startup.sh
